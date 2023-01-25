78462dc4e500c0618fa45b4600fff79aafec0f9f
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-01-25 21:29:13 +0530

    dark mode added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import { createContext, useMemo, useState } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navar';
-
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+export const ColorModeContext = createContext({ toggleColorMode: () => { } });
 function App() {
+
+  const [mode, setMode] = useState('light');
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
   return (
     <div className="App">
-      <Navbar/>
+      <ColorModeContext.Provider value={colorMode}>s
+        <ThemeProvider theme={theme}>
+          <Navbar />
+        </ThemeProvider>
+      </ColorModeContext.Provider>
     </div>
   );
 }
