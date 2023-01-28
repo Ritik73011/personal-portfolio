f32acfbbacceac4dcf368c3bae668f0e0780129f
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-01-28 17:30:46 +0530

    profile photo added

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 import {image, name,summary} from '../../info';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import './About.css'
 const About = () => {
   return (
-    <Box sx={{display:"flex",justifyContent:"space-between"}}>
-        <Box>
+    <Box sx={{display:"flex",justifyContent:"space-around",gap:"16px"}}>
+        <Box sx={{maxWidth:"60%", width:"100%"}}>
             <Typography variant='h4' sx={{textAlign:"left"}}>HELLO</Typography>
             <Typography variant='h3' sx={{textAlign:"left"}}>I am {name}</Typography>
             <Typography sx={{textAlign:"left"}}>{summary}</Typography>
@@ -15,8 +16,10 @@ const About = () => {
                 <LinkedInIcon/>
             </Box>
         </Box>
-        <Box>
-            <img width={'100%'} src={image} alt={name}/>
+        <Box sx={{maxWidth:"40%", width:"100%"}}>
+          <Box sx={{maxWidth:"320px", margin:"auto",height:"90%", width:"100%",borderRadius:"36px",bgcolor:"primary.main"}}>
+            <img className='profile'src={image} alt={name}/>
+          </Box>
         </Box>
     </Box>
   )
--- src/info.js
+++ src/info.js
@@ -1,3 +1,3 @@
 export let summary = 'A Skilled full stack developer and quick learner who can quickly adapt to new environments. Currently, I love to work on web application using technologies like Javascript, React, Chakra UI, Node JS, Express, Mongo DB and Firebase.';
 export let name = 'Ritik Kumar Singh';
-export let image = 'https://firebasestorage.googleapis.com/v0/b/collection-mart.appspot.com/o/DSC_3315.JPG?alt=media&token=f5706b24-b72b-4b7f-b8e6-903da2047947'
+export let image = 'https://firebasestorage.googleapis.com/v0/b/collection-mart.appspot.com/o/DSC_3315-removebg.png?alt=media&token=e5843f3b-47b1-4571-8e3b-66c4e456e63f'
